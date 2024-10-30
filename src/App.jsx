bd1239d3487085478612b6ce22b9be501e5d898f
werik-antunes <127903405+werik-antunes@users.noreply.github.com>
2024-10-29 21:02:20 -0300

    React Galeria de Imagens
    Primeiros passos em React projeto 06 Galeria de Imagens

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import GaleriaDeImagens from './components/GaleriaDeImagens'
+
+function App() {
+  
+
+  return (
+    <>
+     <GaleriaDeImagens/>
+    </>
+  )
+}
+
+export default App
